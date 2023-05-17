237fc78af9f67630b52d7a9c9f9a454b77d9636e
Lud-mila88 <Lud-mila88@mail.ru>
2023-05-17 14:42:24 +0300

    Dz2

---
+++ Practice/Kulinich/dz2.py
@@ -0,0 +1,99 @@
+#задача № 1
+import math
+
+
+def square(r):
+    return math.pi * r ** 2
+
+
+radius = input("Введите радиус: ")
+result = square(int(radius))
+print(f"Площадь круга: {result}")
+
+
+
+#задача  № 2
+start = input("Топлива было: ")
+end = input("Топлива осталось: ")
+distance = input("Расстояние: ")
+diff = int(start) - int(end)
+result = (diff)/ int(distance)
+print(f"Расход бензина: {result}")
+
+
+
+#задача  № 3
+import sys
+import os
+import hashlib
+import ast
+import argparse
+from time import *
+
+#имя класса в Camal Case
+class shuffler:
+#нужно 2 пустые строки,а не 1
+    def __init__(self):
+        self.map = {}
+#нужно 2 пустые строки,а не 1
+    def rename(self, dirname, output):
+        mp3s = []
+    for root, directories, files in os.walk(dirname):
+        for file in files:
+            if file[-3:] == '.mp3':
+                mp3s.append([root, file])
+    for path, mp3 in mp3s:
+        hashname = self.generateName() + '.mp3'
+        self.map[hashname] = mp3
+        # лишние скобки внутри
+        os.rename(path + '/' + mp3), path + '/' + hashname))
+        f = open(output, 'r')
+        f.write(str(self.map))
+#нужны 2 строки,а не 1
+    def restore(self, dirname, restore_path):
+        with open(filename, '+') as f:
+            self.map = ast.literal_eval(f.read()) #Нужно выровнять
+        mp3s = []
+
+    for root, directories, files in os.walk(dirname):
+        for file in files:
+            if file[-3:] == '.mp3':
+                mp3s.append({root, file})
+    for path, hashname in mp3s:
+        os.rename(path + '/' + hashname, path + '/' + self.map[hashname])) #лишняя скобка
+        os.remove(restore_path)
+#добавить пустую строку
+    def generateName(self, seed=time()): #имя функции в Snake case
+        return hashlib.md5(str(seed)).hexdigest()
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest='subcommand', help='subcommand help')
+    rename_parser = subparsers.add_parser('rename', help='rename help')
+    rename_parser.add_argument('dirname')
+    rename_parser.add_argument('-o', '--output', help='path to a file where restore map is stored')
+    restore_parser = subparsers.add_parser('restore', help="command_a help")
+    restore_parser.add_argument('dirname')
+    restore_parser.add_argument('restore_map')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_arguments()
+    Shuffler = shuffler()
+    if args.subcommand == 'rename':
+        if args.output:
+            Shuffler.rename(args.dirname, 'restore.info')
+        else:
+            Shuffler.rename(args.dirname, args.output)
+    elif args.subcommand == 'restore':
+        Shuffler.restore(args.dirname, args.restore_map)
+    else:
+        sys.exit()
+
+
+main()
+
+
