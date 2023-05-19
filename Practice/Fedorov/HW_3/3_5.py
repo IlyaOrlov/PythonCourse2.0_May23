c3f9fea443bf3c7a08cceb5d7a911c8df2df1b56
Андрей <kingfedor@mail.ru>
2023-05-19 19:16:30 +0300

    update 2

--- Practice/Fedorov/HW_3/3_5.py
+++ Practice/Fedorov/HW_3/3_5.py
@@ -1,12 +1,11 @@
-txt = input("Привет! Введи слово, фразу или чиcло, а я скажу палиндром это или нет: ")
 
 
 def preobrazovanie(x):
-    x = x.lower()
-    x = x.replace(" ", "")
+    x = x.lower().replace(" ", "")
     return x
 
 
+txt = input("Привет! Введи слово, фразу или чиcло, а я скажу палиндром это или нет: ")
 txt = preobrazovanie(txt)
 if txt == txt[::-1]:
     print("Это палиндром!")
