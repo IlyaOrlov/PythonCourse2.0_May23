c08809eb6b23a13a90cd064ae741c11552abe0a8
Ksenia Ozerova <ruchkinaks944@gmail.com>
2023-05-20 22:37:45 +0300

    Task 0.Edit 3

--- Practice/Ozerova/3/3.1.py
+++ Practice/Ozerova/3/3.1.py
@@ -1,7 +1,3 @@
-def perimetr(p):
-    return (p)
-
-
 lenght = input("Введите длину прямоугольника: ")
 width = input("Введите ширину прямоугольника: ")
 result = 2*(int(lenght)+int(width))
--- Practice/Ozerova/3/3.2.py
+++ Practice/Ozerova/3/3.2.py
@@ -1,7 +1,3 @@
-def speed(v):
-    return (v)
-
-
 time = input("Введите время движения: ")
 distance = input("Введите расстояние, пройденное за это время: ")
 result = int(distance)/int(time)
