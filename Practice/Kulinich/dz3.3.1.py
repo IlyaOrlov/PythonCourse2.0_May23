2532c30d2cd9fa2352e954feae957308c96c2be4
Lud-mila88 <Lud-mila88@mail.ru>
2023-05-21 17:37:10 +0300

    исправила

--- Practice/Kulinich/dz3.3.1.py
+++ Practice/Kulinich/dz3.3.1.py
@@ -1,6 +1,6 @@
 # зашифровка
 code = input("Введие пароль    ")
 key = 5961
-result = (int(code)^int(key))
-print f"Ваш секретный код:  {result}"
+result = int(code)^int(key)
+print(f"Ваш секретный код:  {result}")
 
