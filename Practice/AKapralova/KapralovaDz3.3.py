c9d924060feaafea86ed3c0386be292feb5a51db
AnnaKapralova <133211547+AnnaKapralova@users.noreply.github.com>
2023-05-22 14:51:47 +0300

    Delete KapralovaDz3.3.py
    Изменила на две новые программы.

--- Practice/AKapralova/KapralovaDz3.3.py
+++
@@ -1,9 +0,0 @@
-password = int(input("Введите цифровой пароль: "))
-key = int(input("Введите код: "))
-result = password ^ key
-print(f"Зашифрованый пароль: {result}")
-
-result = int(input("Введите зашифрованый пароль: "))
-key = int(input("Введите код: "))
-unpassword = result ^ key
-print(f"Расшифрованый пароль: {unpassword}")
