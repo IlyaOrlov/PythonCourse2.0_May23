4d48bf7ca9b94ea9a26e183c90d7276e6d5ac1f9
AnnaKapralova <annyadmitrievna@mail.ru>
2023-05-22 14:45:15 +0300

    Исправлено

--- Practice/AKapralova/KapralovaDz3.4.py
+++ Practice/AKapralova/KapralovaDz3.4.py
@@ -1,4 +1,4 @@
 print("Волшебство: меняем все А на * ")
-str = input("Введите слово: ")
-result = str.replace("А", "*").replace("а", "*")
+a = input("Введите слово: ")
+result = a.replace("А", "*").replace("а", "*")
 print(result)
