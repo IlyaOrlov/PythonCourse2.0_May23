014f8848e251bc58cec839a636923028be20e92d
Lud-mila88 <Lud-mila88@mail.ru>
2023-05-24 13:08:21 +0300

    Исправила 4.1

--- Practice/Kulinich/dz4.1.py
+++ Practice/Kulinich/dz4.1.py
@@ -1,11 +1,11 @@
-i = 0
-while i != 101:
-    i += 1
+i = 1
+while i <= 100:
     if i % 15 == 0:
         print("FizzBuzz")
     elif i % 5 == 0:
         print("Buzz")
-    elif  i % 3 == 0:
+    elif i % 3 == 0:
         print("Fizz")
-    else: print(i)
-
+    else:
+        print(i)
+    i += 1
