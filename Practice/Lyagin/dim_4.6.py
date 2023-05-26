f2dc77c41508bca5e5a139cea9a1968b628718b4
Lyagin Dmitry <dimaly@yandex.ru>
2023-05-26 22:16:07 +0300

    ДЗ 4.6 Edit 1

--- Practice/Lyagin/dim_4.6.py
+++ Practice/Lyagin/dim_4.6.py
@@ -1,14 +1,17 @@
 def maxmin(a, b):
     if a > b:
         print(f"Большее из введённых чисел a = {a}")
+        s = a
     else:
         print(f"Большее из введённых чисел b = {b}")
-    return (a,b)
+        s = b
+    return s
 
 
 a = int(input("Введите число a: "))
 b = int(input("Введите число b: "))
-maxmin (a, b)
+m = maxmin(a, b)
+print(f"Наибольшее число {m}")
 
 
 def minmax(a, b):
