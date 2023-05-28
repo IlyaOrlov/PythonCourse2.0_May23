038803c737cbad12ebacb5340a1b6e86b7a33fa8
ESvetlova <e.as@mail.ru>
2023-05-28 13:59:03 +0300

    исправила замечания по 3.4, 3.. Все гениальное  просто оказывается по задаче 3.5, подскажите теперь как можно было обойтись без s1.

--- Practice/Svetlova/DZ3.5.py
+++ Practice/Svetlova/DZ3.5.py
@@ -1,7 +1,11 @@
 s = input("Введите слово: ")
 s1 = s[::-1]
 
-if s1 == s:
-    print("True")
-else:
-    print("False")
+print(s1 == s)
+
+
+#  оставлю себе как второй варинант решения
+# if s1 == s:
+#     print("True")
+# else:
+#     print("False")
