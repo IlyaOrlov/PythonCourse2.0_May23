574442a392b5444541aba2e01993f6d5ad067325
Lud-mila88 <Lud-mila88@mail.ru>
2023-05-29 23:20:36 +0300

    Task4.6 так?

--- Practice/Kulinich/Dz4.6.py
+++ Practice/Kulinich/Dz4.6.py
@@ -1,18 +1,16 @@
 def myfun1(x,y):
-
-
     if x > y:
         print(x)
     else:
         print(y)
 
+
 def myfun2(x,y):
-
-
     if x > y:
         return x
     else:
         return y
+
 
 a = int(input("Введите 1ое число   "))
 b = int(input("Введите 2ое число   "))
