482807c830712de2ea52bb580cfcd6865aec299a
Lud-mila88 <Lud-mila88@mail.ru>
2023-05-29 23:47:48 +0300

    надеюсь правильно поняла

--- Practice/Kulinich/Dz4.5.py
+++ Practice/Kulinich/Dz4.5.py
@@ -4,15 +4,12 @@ import random
 x = input("Введите минимальное число диапазона от  ")
 y = input("Введите максимальное число диапазона до  ")
 v = random.randint(int(x),int(y))
-while (z := input("Угадайте число  ")) != str(v):
-    if not z.isnumeric():
-        input("Введен не числовой символ  ")
-        break
+while (z := input("Угадайте число  ")).isnumeric():
+    if int(z) == v:
+        input("Вы угадали  ")
     elif int(z) < v:
         print('Нет, загаданное число немного больше этого.')
     else:
         print('Нет, загаданное число немного меньше этого.')
-
-
 
 
--- Practice/Kulinich/Dz4.8.py
+++ Practice/Kulinich/Dz4.8.py
@@ -1,23 +1,26 @@
 import random
 
-srt = input("Сделайте выбор: камень, ножницы , бумага?   ")
+srt = input("Сделайте выбор: камень, ножницы , бумага?   ").lower()
 lst = ["камень" , "ножницы" , "бумага"]
-x = random.choices(lst, k=1)[0]
+x = random.choice(lst)
 print(f'Я загадал {x}')
 if srt == x:
     print("Ничья")
-elif srt == "камень" and x == "ножницы":
-    print("Ты выиграл")
-elif srt == "камень" and x == "бумага":
-    print("Ты проиграл")
-elif srt == "ножницы" and x == "бумага":
-    print("Ты выиграл")
-elif srt == "ножницы" and x == "камень":
-    print("Ты проиграл")
-elif srt == "бумага" and x == "камень":
-    print("Ты выиграл")
-elif srt == "бумага" and x == "ножницы":
-    print("Ты проиграл")
+elif srt == "камень":
+    if x == "ножницы":
+        print("Ты выиграл")
+    else:
+        print("Ты проиграл")
+elif srt == "ножницы":
+    if x == "бумага":
+        print("Ты выиграл")
+    else:
+        print("Ты проиграл")
+elif srt == "бумага":
+    if x == "камень":
+        print("Ты выиграл")
+    else:
+        print("Ты проиграл")
 else:
     print("Некорретно введено слово")
 
