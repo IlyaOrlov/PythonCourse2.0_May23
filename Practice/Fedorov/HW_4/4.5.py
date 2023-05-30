b05d2407784f2d0c1e77bdede2fea617f4d0fb45
Андрей <kingfedor@mail.ru>
2023-05-31 00:18:54 +0300

    поправил замечания [4]

--- Practice/Fedorov/HW_4/4.5.py
+++ Practice/Fedorov/HW_4/4.5.py
@@ -7,19 +7,20 @@ def numprov(x, txt):
     return int(x)
 
 
-numStart = numprov("text", "минимальное")
-numEnd = numprov("text", "максимальное")
-numTrue = random.randint(numStart, numEnd)
+num_start = numprov("text", "минимальное")
+num_end = numprov("text", "максимальное")
+num_true = random.randint(num_start, num_end)
 # для отладки print(numTrue)
-numGuess = -1
-while int(numGuess) != numTrue:
-    numGuess = input("Какое число вы загадали?")
-    if not numGuess.isnumeric():
+num_guess = -1
+while int(num_guess) != num_true:
+    num_guess = input("Какое число вы загадали?")
+    if not num_guess.isnumeric():
         print("Увы и ах, возвращайся если захочешь ещё поиграть.")
         break
-    if int(numGuess) == numTrue:
+    i_num_guess = int(num_guess)
+    if i_num_guess == num_true:
         print("Поздравляю, ты угадал загаданное число!")
-    elif int(numGuess) < numTrue:
-        print(f"Загаданное число больше чем {numGuess}.")
+    elif i_num_guess < num_true:
+        print(f"Загаданное число больше чем {num_guess}.")
     else:
-        print(f"Загаданное число меньше чем {numGuess}.")
+        print(f"Загаданное число меньше чем {num_guess}.")
--- Practice/Fedorov/HW_4/4.6.py
+++ Practice/Fedorov/HW_4/4.6.py
@@ -8,8 +8,12 @@ def poisk_max(num1, num2):
     return x
 
 
-def print_max(val):
-    print(val)
+def print_max(num1, num2):
+    if num1 > num2:
+        print(num1)
+    else:
+        print(num2)
 
+poisk_max(14, 20)
+print_max(14, 20)
 
-print_max(poisk_max(14, 20))
