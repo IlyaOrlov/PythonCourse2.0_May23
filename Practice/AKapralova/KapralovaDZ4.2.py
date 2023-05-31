326f86f5053c851d88129d23e3b9a79a834d094e
AnnaKapralova <annyadmitrievna@mail.ru>
2023-05-31 13:29:49 +0300

    Исправила

--- Practice/AKapralova/KapralovaDZ4.2.py
+++ Practice/AKapralova/KapralovaDZ4.2.py
@@ -1,11 +1,9 @@
-a = input("Введите пятизначное число: ")
-while a != len(a) != 5 or not a.isdigit():
+while len((a := input("Введите пятизначное число: "))) != 5 or not a.isdigit():
     if len(a) != 5 or not a.isdigit():
         print("Число не пятизначное или это не число.")
 
-    a = input("Введите пятизначное число: ")
 
 else:
     print(f"Число: {a}")
-    for i, j in enumerate(a):
-        print(f"{i+1} цифра ровна {j}")
+    for i, j in enumerate(a, 1):
+        print(f"{i} цифра ровна {j}")
