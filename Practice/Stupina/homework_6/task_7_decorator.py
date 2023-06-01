3d89e4b66a3a55851b27b1e4001e66e110ebd4e9
Alena <alenadv91@mail.ru>
2023-06-01 23:39:02 +0300

    домашнее задание №6,исправлено

--- Practice/Stupina/homework_6/task_7_decorator.py
+++ Practice/Stupina/homework_6/task_7_decorator.py
@@ -1,15 +1,13 @@
 def wrapper(fun):
-#здесь же должны быть 2 строки пустые (отделяем ф-ию верхнего уровня)?
-
     def new_fun(*args, **kwargs):
         print('==========')
         res = fun(*args, **kwargs)
         print(res)
         print('==========')
         return res
-
     return new_fun
-#  а здесь одна?
+
+
 def sum(x, y):
     return x + y
 
