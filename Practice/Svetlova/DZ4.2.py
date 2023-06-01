b54f14e4d3488e6c0704af598fe7ec0920117353
ESvetlova <e.as@mail.ru>
2023-06-01 19:55:56 +0300

    Добавила ДЗ 4.3., прошу проверить.

--- Practice/Svetlova/DZ4.2.py
+++ Practice/Svetlova/DZ4.2.py
@@ -1,4 +1,4 @@
 x = input("Введите пятизначное число: ")
-if x.isnumeric() and len(x)==5:
+if x.isnumeric() and len(x) == 5:
     for i in range(len(x)):
         print(f"{i + 1} цифра равна {x[i]}")
