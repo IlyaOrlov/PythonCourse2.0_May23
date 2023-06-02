cf30a371614dc5bb3918d25b23e817f5d2e364c1
ESvetlova <e.as@mail.ru>
2023-06-02 08:58:45 +0300

    дополнила 4.2, если правильно поняла Ваш комментарий, прошу проверить.

--- Practice/Svetlova/DZ4.2.py
+++ Practice/Svetlova/DZ4.2.py
@@ -2,3 +2,5 @@ x = input("Введите пятизначное число: ")
 if x.isnumeric() and len(x) == 5:
     for i, n in enumerate(x, 1):
         print(f"{i} цифра равна {n}")
+if not x.isdigit():
+    print("Предупреждение: Введен нечисловой символ")
