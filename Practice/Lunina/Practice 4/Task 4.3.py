bb8dfa09b1174fc707a4d304b85496181ce2b183
OLunina <ovknyaz4@gmail.com>
2023-06-02 16:17:43 +0300

    Task 4.3 v1

--- Practice/Lunina/Practice 4/Task 4.3.py
+++ Practice/Lunina/Practice 4/Task 4.3.py
@@ -2,9 +2,8 @@ n = ""
 while (i := input("Введите число: ")).lower() != "stop":
     if i.isdigit():
         print(f" Число: {i}")
+        n += i
     else:
         print("Ошибка! Введите только число ")
-        continue
-    n += i
 
 print(f"Вы ввели {n}. До встречи.")
