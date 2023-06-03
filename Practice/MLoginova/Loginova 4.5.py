522ec97f4025e111402e1dd014de65a479d368c2
Maria Loginova <marria_loginova@mail.ru>
2023-06-03 16:29:33 +0300

    Task 4.5. Edit 2

--- Practice/MLoginova/Loginova 4.5.py
+++ Practice/MLoginova/Loginova 4.5.py
@@ -2,29 +2,23 @@ import random
 
 
 def enter_chislo():
-    while not (a := input("Введите число: ")).isdecimal():
+    while not (a := input("Введите число: ")).isnumeric():
         print("Вы ввели НЕ число")
     return int(a)
 
+
 print("Привет! Давай ты задашь диапазон, я загадаю число из этого диапазона, а ты попробуешь его отгадать. Поехали!")
-x = enter_chislo()
-y = enter_chislo()
-#проверка на совпадение нижней и верхней границы
-while x == y:
+while (x := enter_chislo()) == (y := enter_chislo()):
     print("Нижняя и верхняя граница диапазона совпадают. Повторите ввод: ")
-    x = enter_chislo()
-    y = enter_chislo()
 if x < y:
     n = random.randint(x, y)
     print("Загадано число в диапазоне от {} до {}, попробуй отгадать какое:".format(x, y))
 else:
     n = random.randint(y, x)
     print("Загадано число в диапазоне от {} до {}, попробуй отгадать какое:".format(y, x))
-while poisk := enter_chislo():
-    if poisk > n:
-        print("Число больше загаданного")
-    elif poisk < n:
-        print("Число меньше загаданного")
-    elif poisk == n:
-         print("Поздавляем! Вы угадали, это число - {} ".format(poisk))
-         break
+while (p := enter_chislo()) != n:
+    if p > n:
+        print(f"Число {p} больше загаданного")
+    else:
+        print(f"Число {p} меньше загаданного")
+print("Поздавляем! Вы угадали, это число - {} ".format(p))
