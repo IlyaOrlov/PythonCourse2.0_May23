678f2abd94aaba2d93318cd5fc89f803a281ec1e
OLunina <ovknyaz4@gmail.com>
2023-06-03 13:57:02 +0300

    Task 4.2 finish

---
+++ Practice/Lunina/Practice 4/Task 4.6.py
@@ -0,0 +1,27 @@
+def myf1(x, y):
+    print(f"Вы ввели: {x } и {y }. Сравним.")
+    if x > y:
+        print(f"Наибольшее значение в паре {x}.")
+    elif x == y:
+        print(f"Введенные числа равны. {x} = {y}")
+    else:
+        print(f"Наибольшее значение в паре {y}.")
+
+def myf2(x, y):
+    if x > y:
+        return x
+    elif x == y:
+        return x == y
+    else:
+        return y
+
+a = int(input(f"Введите любое число: "))
+b = int(input(f"Введите любое число: "))
+
+myf1(a, b)
+
+res = myf2(a, b)
+print(f"Наибольшее  возвращаемое значение {res}")
+
+
+
