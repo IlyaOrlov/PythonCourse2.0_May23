4338e6f7a374892b611a0715d8dd68fc0e20ac9c
Dmitry Sapegov <sapegych@mail.ru>
2023-06-04 21:53:01 +0300

    Задача 5.1 + правка 5.2

--- Practice/Sapegov/Sap5.2.py
+++ Practice/Sapegov/Sap5.2.py
@@ -1,9 +1,8 @@
 def fun(x):
     for i in range(len(x)):
         for j in range(i):
-            if i != j:
-                if x[i] == x[j]:
-                    return x[i]
+            if x[i] == x[j]:
+                return x[i]
 
 
 lst1 = [2, 3, 4, 5, 3, 2]
