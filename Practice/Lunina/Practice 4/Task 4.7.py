a366828f7ead8d1b29ccea550c9b700d82db76c8
OLunina <ovknyaz4@gmail.com>
2023-06-05 16:28:16 +0300

    Task 4.7 start

---
+++ Practice/Lunina/Practice 4/Task 4.7.py
@@ -0,0 +1,40 @@
+def outer_decor(fun):
+    def inner_decor(*args, **kwargs):
+        print("======")
+        result = fun(*args, **kwargs)
+        print("======")
+        return result
+
+    return inner_decor
+
+
+@outer_decor
+def myf1(x, y):
+    print(f"Вы ввели: {x} и {y}")
+    if x > y:
+        print(f"Наибольшее значение в паре {x}.")
+    elif x == y:
+        print(f"Введенные числа равны. {x} = {y}")
+    else:
+        print(f"Наибольшее значение в паре {y}.")
+
+
+def myf2(x, y):
+    if x >= y:
+        return x
+    else:
+        return y
+
+while not (a := input(f"Введите 1 число: ")).isdecimal() \
+        or not (b := input(f"Введите 2 число: ")).isdecimal():
+    print("Вы ввели не число.")
+
+else:
+    print("Отлично! Приступим к сравнению.")
+    a = int(a)
+    b = int(b)
+    myf1(a, b)
+
+
+res = myf2(a, b)
+print(f"Наибольшее  возвращаемое значение {res}")
