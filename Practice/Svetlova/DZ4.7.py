1107902ddc303ee67794d324f1ae103bb7a2bda2
ESvetlova <e.as@mail.ru>
2023-06-05 16:39:24 +0300

    Добавила задачу 4.7

---
+++ Practice/Svetlova/DZ4.7.py
@@ -0,0 +1,13 @@
+def outer(fun):
+    def inner(*args, **kwargs):
+        print('=========')
+        res = fun(*args, **kwargs)
+        print('=========')
+        return res
+    return inner
+@outer
+def s_fun():
+    print("Hello")
+
+
+s_fun()
