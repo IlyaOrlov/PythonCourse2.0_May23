5cd524559862b47a02af474e35bebd4d41c0e22e
ESvetlova <e.as@mail.ru>
2023-06-06 16:55:56 +0300

    Добавила задачу 5 к лекции 4, прошу проверить.

---
+++ Practice/Svetlova/DZ4.5.py
@@ -0,0 +1,20 @@
+import random
+
+def guess_number():
+    x = int(input("Введите минимальное число диапазона: "))
+    y = int(input("Введите максимальное число диапазона: "))
+
+    secret_number = random.randint(x, y)
+
+    while True:
+        guess = int(input("Угадайте число: "))
+
+        if guess == secret_number:
+            print("Поздравляю! Вы угадали число.")
+            break
+        elif guess < secret_number:
+            print("Загаданное число больше.")
+        else:
+            print("Загаданное число меньше.")
+
+guess_number()
