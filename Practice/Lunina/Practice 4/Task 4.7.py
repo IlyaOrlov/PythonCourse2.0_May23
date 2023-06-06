6e5f5fbb13a4fb307eb4255a6da3f59b4478c4df
OLunina <ovknyaz4@gmail.com>
2023-06-06 07:08:03 +0300

    Task 4.7 v1

--- Practice/Lunina/Practice 4/Task 4.7.py
+++ Practice/Lunina/Practice 4/Task 4.7.py
@@ -19,22 +19,11 @@ def myf1(x, y):
         print(f"Наибольшее значение в паре {y}.")
 
 
-def myf2(x, y):
-    if x >= y:
-        return x
-    else:
-        return y
-
 while not (a := input(f"Введите 1 число: ")).isdecimal() \
         or not (b := input(f"Введите 2 число: ")).isdecimal():
     print("Вы ввели не число.")
 
 else:
-    print("Отлично! Приступим к сравнению.")
     a = int(a)
     b = int(b)
     myf1(a, b)
-
-
-res = myf2(a, b)
-print(f"Наибольшее  возвращаемое значение {res}")
