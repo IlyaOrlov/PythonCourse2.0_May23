43f63385e2f1c653aa59ce8108e1cb15c61608c3
mihailkisunkin <mkisunkin@gmail.com>
2023-06-07 23:08:59 +0300

    второе и шестое задание к 4 лекции

--- Practice/Kisunkina/lec 4/homework4.2.py
+++ Practice/Kisunkina/lec 4/homework4.2.py
@@ -1,13 +1,12 @@
 len_str = 5
-i = 0
 
 while True:
-    number = input("Введите пятизначное число: ")
+    number = input(f"Введите {len_str}-значное число: ")
     if len(number) == len_str:
+        i = 0
         while i < 5:
             print(f"{i+1} цифра равна {number[i]}")
             i += 1
         break
     else:
-        len(number) != len_str
         print(f"Введена неверная длина строки, введите {len_str}-значное число")
--- Practice/Kisunkina/lec 4/homework4.6.py
+++ Practice/Kisunkina/lec 4/homework4.6.py
@@ -3,19 +3,23 @@ def fun1(num1, num2):
         print(f"Первая функция. Вывожу на экран большее число: {num1}")
     elif num1 < num2:
         print(f"Первая функция. Вывожу на экран большее число: {num2}")
+    else:
+        print("Значения равны между собой - я не могу вывести большее")
 
-def fun2(num1, num2):
-    if num1 > num2:
-        x = num1
-    elif num2 > num1:
-        x = num2
+
+def fun2(number_1, number2):
+    x = None
+    if number_1 > number2:
+        x = number_1
+    elif number2 > number_1:
+        x = number2
+    else:
+        print("Значения равны между собой - я не могу вывести большее")
     return x
-
 
 print("Эта программа должна вывести на экран большее из двух введённых чисел с помощью двух функций")
 num1 = int(input("Введите первое число: "))
 num2 = int(input("Введите второе число: "))
-x = fun2(num1, num2)
 fun1(num1, num2)
 fun2(num1, num2)
-print(f"Вторая функция. Большее число: {x}")
+
