88b00cad8c9e14e29ec18f76d6d5f329d0412f23
mihailkisunkin <mkisunkin@gmail.com>
2023-06-07 11:43:51 +0300

    шестое задание к 4 лекции

--- Practice/Kisunkina/lec 4/homework4.2.py
+++ Practice/Kisunkina/lec 4/homework4.2.py
@@ -1,9 +1,13 @@
-number = input("Введите пятизначное число: ")
+len_str = 5
+i = 0
 
-if len(number) == 5:
-    i = 0
-    while i < 5:
-        print(f"{i+1} цифра равна {number[i]}")
-        i += 1
-else:
-    print("Введена неверная длина строки, введите пятизначное число")
+while True:
+    number = input("Введите пятизначное число: ")
+    if len(number) == len_str:
+        while i < 5:
+            print(f"{i+1} цифра равна {number[i]}")
+            i += 1
+        break
+    else:
+        len(number) != len_str
+        print(f"Введена неверная длина строки, введите {len_str}-значное число")
