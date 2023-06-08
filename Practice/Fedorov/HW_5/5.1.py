19ea810b96ef7c035a1bc27b058035d229d2f364
Андрей <kingfedor@mail.ru>
2023-06-08 12:33:07 +0300

    upd 6

--- Practice/Fedorov/HW_5/5.1.py
+++ Practice/Fedorov/HW_5/5.1.py
@@ -1,5 +1,6 @@
 def minim(x):
     num_min = float("inf")
+    idx_min = float("inf")
     for index, i in enumerate(x):
         if i < num_min:
             num_min = i
@@ -9,9 +10,10 @@ def minim(x):
 
 arr = [0, 54, 55, 53, 100, 51, 0, 8, 9, 0]
 
+
 for index, _ in enumerate(arr):
     num_min, idx = minim(arr[index:])
-    if idx != 0:
+    if idx != 0 and idx != float("inf"):
         arr[idx + index] = arr[index]
         arr[index] = num_min
 
