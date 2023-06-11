5a1e9f8278af5057c4c6956aa0f6ddbad49ce8c9
Lyagin Dmitry <dimaly@yandex.ru>
2023-06-11 16:00:13 +0300

    ДЗ 5.1 Edit2 + 5.2 Edit1

--- Practice/Lyagin/dim_5.1.py
+++ Practice/Lyagin/dim_5.1.py
@@ -1,14 +1,49 @@
 
-def min_el(arr):
-    for i in range(len(arr)):
-        for k in range(i):
-            if arr[k] > arr[i]:
-                arr[k],arr[i] = arr[i],arr[k]
-    return(arr)
+# def min_el(arr):
+#     for i in range(len(arr)):
+#         for k in range(i):
+#             if arr[k] > arr[i]:
+#                 arr[k],arr[i] = arr[i],arr[k]
+#     return arr
+
+
+arr = [0, 3, 24, 2, 3, 7]
+# print(arr)
+# print(min_el(arr))
+j = 0
+for i in range(len(arr)):
+    if arr[i] <= arr[j]:
+        arr[i],arr[j] = arr[j],arr[i]
+        print(arr[i])
+        print(arr)
+j += 1
+for i in range(j,len(arr)):
+    if arr[i] <= arr[j]:
+        arr[i],arr[j] = arr[j],arr[i]
+        print(arr[i])
+        print(arr)
+
+j += 1
+for i in range(j,len(arr)):
+    if arr[i] <= arr[j]:
+        arr[i],arr[j] = arr[j],arr[i]
+        print(arr[i])
+        print(arr)
+
+j += 1
+for i in range(j,len(arr)):
+    if arr[i] <= arr[j]:
+        arr[i],arr[j] = arr[j],arr[i]
+        print(arr[i])
+        print(arr)
+
+j += 1
+for i in range(j,len(arr)):
+    if arr[i] <= arr[j]:
+        arr[i],arr[j] = arr[j],arr[i]
+        print(arr[i])
+        print(arr)
 
 
 
 
-arr = [0, 3, -8, 2, 3, 7]
-print(arr)
-print(min_el(arr))
