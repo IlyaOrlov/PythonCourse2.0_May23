49438d15ce38652e0a77ff2d235f81f952906022
AnnaKapralova <annyadmitrievna@mail.ru>
2023-06-12 13:26:03 +0300

    Исправила

--- Practice/AKapralova/KapralovaDz5.2.py
+++ Practice/AKapralova/KapralovaDz5.2.py
@@ -1,9 +1,9 @@
 def repeat(x):
-    lst1 = list()
+    lst1 = set()
     for i in lst:
         if i in lst1:
             return i
-        lst1.append(i)
+        lst1.add(i)
 
 
 lst = [2, 3, 4, 5, 3, 2]
