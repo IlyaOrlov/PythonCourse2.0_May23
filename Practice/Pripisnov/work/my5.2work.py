98581d77c5475fac6d8d2d0920d19dea68476638
Pripisnov <prialexey@mail.ru>
2023-06-12 13:57:30 +0300

    Лекция5

---
+++ Practice/Pripisnov/work/my5.2work.py
@@ -0,0 +1,16 @@
+def find_first_duplicate(lst):
+
+
+    x = set()
+
+    for i in lst:
+        if i in x:
+            return i
+        x.add(i)
+
+    return None
+
+# Пример функции
+my_list = [2, 3, 4, 5, 3, 2]
+result = find_first_duplicate(my_list)
+print("Первый повторяющийся символ:", result)
