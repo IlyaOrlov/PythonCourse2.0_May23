a5b8beee84b670109b0413a942d710d5d31cd9b9
Maria Loginova <marria_loginova@mail.ru>
2023-06-15 17:55:48 +0300

    Tasks 5.5. Edit 1 Внесла исправления + в стр.13 и стр.20 добавила досрочный выход из программы, чтобы не было лишней записи в файл, когда табуляции и 4 пробелов нету в файле

--- Practice/MLoginova/Loginova_5.5.py
+++ Practice/MLoginova/Loginova_5.5.py
@@ -1,29 +1,31 @@
-def file_actions(f_name, k):
-    with open(f_name, 'r', encoding="utf8") as f:
+import sys
+
+def file_actions(file, choice, a, b):
+    with open(file, 'r', encoding="utf8") as f:
         data = f.read()
         #print(data)
         if k == 1:
             if a in data:
                 data = data.replace(a, b)
-                print(f"В файле {f_name} присутствует символ(ы) табуляции и он(и) заменены на 4 пробела")
+                print(f"В файле {file} присутствует символ(ы) табуляции и он(и) заменены на 4 пробела")
             else:
-                print(f"В файле {f_name} отсутствует символ(ы) табуляции, закрываем файл")
-                f.close()
+                print(f"В файле {file} отсутствует символ(ы) табуляции, закрываем файл")
+                sys.exit(0)
         else:
             if b in data:
                 data = data.replace(b, a)
-                print(f"В файле {f_name} присутствуеn(-ют) 4 пробела и он(и) заменены на символ табуляции")
+                print(f"В файле {file} присутствуеn(-ют) 4 пробела и он(и) заменены на символ табуляции")
             else:
-                print(f"В файле {f_name} отсутствует символы  - 4 пробела, закрываем файл")
-                f.close()
-    with open(f_name, 'w', encoding="utf8") as f:
+                print(f"В файле {file} отсутствует символы  - 4 пробела, закрываем файл")
+                sys.exit(0)
+    with open(file, 'w', encoding="utf8") as f:
         f.write(data)
 
 
-f_name = 'primer.txt'
-a = '\t'
-b = '    '
+FILE_NAME = 'primer.txt'
+A = '\t'
+B = '    '
 t = (1, 2)
-while (k := int(input(f"Выбери, действие с файлом {f_name}:\n1 - заменить все табуляции на 4 пробела\n2 - или заменить 4 пробела на табуляцию\n>>:"))) not in t:
+while (k := int(input(f"Выбери, действие с файлом {FILE_NAME}:\n1 - заменить все табуляции на 4 пробела\n2 - или заменить 4 пробела на табуляцию\n>>:"))) not in t:
     print("Повторите ввод!:")
-file_actions(f_name, k)
+file_actions(FILE_NAME, k, A, B)
