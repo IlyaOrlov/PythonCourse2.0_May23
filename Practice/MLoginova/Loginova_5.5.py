998bb171decc7c48de738b512f5f2ba36fa2c4a6
Maria Loginova <marria_loginova@mail.ru>
2023-06-16 21:01:22 +0300

    Task 5.5. Edit 2

--- Practice/MLoginova/Loginova_5.5.py
+++ Practice/MLoginova/Loginova_5.5.py
@@ -1,31 +1,29 @@
-import sys
-
-def file_actions(file, choice, a, b):
+def file_actions(file, choice, tab, spase):
     with open(file, 'r', encoding="utf8") as f:
         data = f.read()
         #print(data)
         if k == 1:
-            if a in data:
-                data = data.replace(a, b)
+            if tab in data:
+                data = data.replace(tab, spase)
                 print(f"В файле {file} присутствует символ(ы) табуляции и он(и) заменены на 4 пробела")
             else:
                 print(f"В файле {file} отсутствует символ(ы) табуляции, закрываем файл")
-                sys.exit(0)
+                return
         else:
-            if b in data:
-                data = data.replace(b, a)
+            if spase in data:
+                data = data.replace(spase, tab)
                 print(f"В файле {file} присутствуеn(-ют) 4 пробела и он(и) заменены на символ табуляции")
             else:
                 print(f"В файле {file} отсутствует символы  - 4 пробела, закрываем файл")
-                sys.exit(0)
+                return
     with open(file, 'w', encoding="utf8") as f:
         f.write(data)
 
 
 FILE_NAME = 'primer.txt'
-A = '\t'
-B = '    '
+TAB_SYM = '\t'
+SPACE_SYM = '    '
 t = (1, 2)
 while (k := int(input(f"Выбери, действие с файлом {FILE_NAME}:\n1 - заменить все табуляции на 4 пробела\n2 - или заменить 4 пробела на табуляцию\n>>:"))) not in t:
     print("Повторите ввод!:")
-file_actions(FILE_NAME, k, A, B)
+file_actions(FILE_NAME, k, TAB_SYM, SPACE_SYM)
