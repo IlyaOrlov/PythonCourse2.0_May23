9e0927e0cc9c113ed8f13b1f721ccec298fa19fd
Maria Loginova <marria_loginova@mail.ru>
2023-06-19 17:21:34 +0300

    Tasks 6.1.Edit 0

---
+++ Practice/MLoginova/Loginova_6.1.py
@@ -0,0 +1,43 @@
+import random
+import datetime
+
+class Tanks:
+    model = "_"
+    country = "_"
+    price = 0
+
+    def __init__(self, model_):
+        self.model = model_
+        self.country = "_"
+        self.price = 0
+
+    def random_country(self, list_country):
+        country = random.choice(list_country)
+        return country
+
+    #стоимость танка в рублях(стоимость танка в долларах * на курс)
+    def price_tank(self, kurs_usd):
+        price = PRICE_TANK_USB * kurs_usd
+        return price
+
+
+    def info_tanks(self):
+        dt_now = datetime.date.today()
+        print(f"Модель танка {self.model}:\nCтрана {self.random_country(list_country)}, стоимость на дату {dt_now} в рублях: {self.price_tank(kurs_usd)}.\n")
+
+
+list_country = ["Russia", "USA", "Gourmania", "Ukraine", "Poland"]
+# пока не знаю как курс взять с сайта ЦБ РФ, пусть его введёт пользователь для дальнейшего расчёта стоимости танка в руб.:
+kurs_usd = float(input("Enter the course 'USD' for today: "))
+PRICE_TANK_USB = 2500000
+
+t1 = Tanks("T-90")
+t1.info_tanks()
+
+
+t2 = Tanks("Тигр")
+t2.info_tanks()
+
+
+t3 = Tanks("Пантера")
+t3.info_tanks()
