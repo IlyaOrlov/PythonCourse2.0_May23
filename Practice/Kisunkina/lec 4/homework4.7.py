87143521001ee7aa2a2380888e46abd617bead74
mihailkisunkin <mkisunkin@gmail.com>
2023-06-27 16:09:00 +0300

    задача семь к четвертой лекции

---
+++ Practice/Kisunkina/lec 4/homework4.7.py
@@ -0,0 +1,16 @@
+# Написать декоратор, выводящий "===========" до и после запуска функции.
+
+def decor_fun(function_to_decorate):
+    def inner_fun(*args, **kwargs):
+        print('"==========="')
+        res = function_to_decorate(*args, **kwargs)
+        print('"==========="')
+        return res
+    return inner_fun()
+
+
+def alone():
+    print("Я просто функция, для проверки")
+
+
+alone = decor_fun(alone)
