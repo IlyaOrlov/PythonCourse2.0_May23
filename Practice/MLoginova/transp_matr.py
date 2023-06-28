4884795b4d217f8ffd7bda47eb99fea297657e97
Maria Loginova <marria_loginova@mail.ru>
2023-06-28 09:53:00 +0300

    Tasks 6.1, Edit 0

--- Practice/MLoginova/transp_matr.py
+++ Practice/MLoginova/transp_matr.py
@@ -8,8 +8,8 @@ print(f"Исходная матрица{lst}")
    # a.append([0] * len(lst))
 a = [[0] * len(lst) for i in range(len(lst)+1)]
 print(a)
-for i in range(len(lst)):
-    for j in range(3):
-        a[j][i] = lst[i][j]
-#a[j][i] = [lst[i][j] for i in range(len(lst)) for j in range(3)]
+#for i in range(len(lst)):
+   # for j in range(3):
+        #a[j][i] = lst[i][j]
+a = [[lst[i][j] for i in range(len(lst)) for j in range(len(lst[0]))]]
 print(a)
