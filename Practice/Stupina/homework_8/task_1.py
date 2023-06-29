a6057678edeb9381aa179bfc8a23edf593a703b7
Alena <alenadv91@mail.ru>
2023-06-29 08:42:38 +0300

    дз №8, исправлено

--- Practice/Stupina/homework_8/task_1.py
+++ Practice/Stupina/homework_8/task_1.py
@@ -1,38 +1,32 @@
 class MyIter:
-    _p = '\u00a7'
+    __p = '\u00a7'
 
     def __init__(self, text):
         self._text = text
-        self.i = self._text.find(self._p, 0)
-        self.s_ex = 'Символ не найден'
+        self._i = self._text.find(self.__p, 0)
+        self.__s_ex = 'Символ не найден'
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.i != -1:
-            j = self._text.find(self._p, self.i + 1)
+        if self._i != -1:
+            j = self._text.find(self.__p, self._i + 1)
             if j != -1:
-                res = self._text[self.i:j - 1]
+                res = self._text[self._i:j - 1]
             else:
-                res = self._text[self.i:]
-                self.s_ex = 'Конец текста'
-            self.i = j
+                res = self._text[self._i:]
+                self.__s_ex = 'Конец текста'
+            self._i = j
             return res
         else:
-            raise StopIteration(self.s_ex)
+            pass
+            raise StopIteration(self.__s_ex)
 
 
 s = '§Зайка\nЗайку бросила хозяйка -\nПод дождем остался зайка.\nСо скамейки слезть не смог,\nВесь до ниточки промок.' \
    '\n§Бычок\nИдет бычок, качается,\nВздыхает на ходу:\n- Ох, доска кончается,\nСейчас я упаду' \
    '\n§Лошадка\nЯ люблю свою лошадку,\nПричешу ей шерстку гладко,\nГребешком приглажу хвостик\nИ верхом поеду в гости'
 
-ob = MyIter(s)
-it = iter(ob)
-while True:
-    try:
-        print(next(it))
-        print('------')
-    except StopIteration as ex:
-        print(ex)
-        break
+for i in MyIter(s):
+    print(i)
--- Practice/Stupina/homework_8/task_4.py
+++ Practice/Stupina/homework_8/task_4.py
@@ -1,19 +1,15 @@
-from itertools import *
+import itertools
 
 
 # задание 1
-lst_1 = []
-for i in chain([1, 2, 3], [4, 5], [6, 7]):
-    lst_1.append(i)
+lst_1 = list(itertools.chain([1, 2, 3], [4, 5], [6, 7]))
 print(lst_1)
 
 # задание 2
 lst_in = ['hello', 'i', 'write', 'cool', 'code']
-lst_2 = []
-for i in filterfalse(lambda x: len(x) < 5, lst_in):
-    lst_2.append(i)
+lst_2 = list(itertools.filterfalse(lambda x: len(x) < 5, lst_in))
 print(lst_2)
 
 # задание 3
-for i in permutations('password', 4):  # возможно я неправильно поняла,что нужно вывести
-    print(i)
+lst_3 = list(itertools.combinations('password', 4))
+print(lst_3)
