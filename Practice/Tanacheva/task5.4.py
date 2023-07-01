3353acadbb360957c8ce150ab5c73fbfa7f28512
Julia Tanacheva <jtanacheva@gmail.com>
2023-07-01 22:03:42 +0300

    fix 5.4, удаление в одну строку не получилось

--- Practice/Tanacheva/task5.4.py
+++ Practice/Tanacheva/task5.4.py
@@ -8,20 +8,17 @@ def del_column(m, n):
     m = trans_matrix_fun(m)
     i = 0
     while i < len(m):
-        j = 0
-        while j < len(m[i]):
-            if m[i][j] == n:
-                del (m[i])
-                i = 0
-                j = 0
-            else:
-                j += 1
-        i += 1
-    m = trans_matrix_fun(m)
+        if n in m[i]:
+            del m[i]
+        else:
+            i += 1
+    if m != []:
+        m = trans_matrix_fun(m)
     return m
 
 
-matrix = [[4, 2, 3], [4, 4, 6], [7, 8, 9], [4, 7, 90]]
+matrix = [[4, 4], [4, 3]]
 number = int(input("Введите число для удаления:"))
+
 matrix = del_column(matrix, number)
 print(matrix)
