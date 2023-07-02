bff35fe20d1ceed4b38720ba73487447d4ef9145
Maria Loginova <marria_loginova@mail.ru>
2023-07-02 15:52:54 +0300

    Tasks 7.3 Edit 2

--- Practice/MLoginova/Loginova_7.3.py
+++ Practice/MLoginova/Loginova_7.3.py
@@ -1,4 +1,5 @@
 class MainATM:
+    __NAME_BANK = "ТСБ"
 
     def __init__(self, id_atm, balance=0):
         self._id_atm = id_atm
@@ -6,7 +7,7 @@ class MainATM:
 
     def withdraw_money(self):
         print("******************************************************")
-        money = int(input(f"ID банкомата:{self._id_atm} Выбрана операция снятие наличных."
+        money = int(input(f"{self.__NAME_BANK} ID банкомата:{self._id_atm} Выбрана операция снятие наличных."
                           f"\nУкажите сумму, которую хотите снять: "))
         if self._balance > money:
             self._balance -= money
@@ -16,16 +17,17 @@ class MainATM:
 
     def deposit_money(self):
         print("******************************************************")
-        money = int(input(f"ID банкомата:{self._id_atm} Выбрана операция пополнения наличных."
+        money = int(input(f"{self.__NAME_BANK} ID банкомата:{self._id_atm} Выбрана операция пополнения наличных."
                           f"\nУкажите сумму, которую хотите внести: "))
         self._balance += money
         print(f"Внесена сумма  - {money}")
 
-    def inf_atm(self):
+    def inf_balance(self):
         print("******************************************************")
-        print(f"ID банкомата:{self._id_atm}, баланс - {self._balance}.")
+        print(f"{self.__NAME_BANK} ID банкомата:{self._id_atm}, баланс - {self._balance}.")
 
-    def inf1_atm(self):
+    @staticmethod
+    def list_operation():
         print(f"Cписок поддерживаемых операций: \nПросмотр баланса\nВыдача наличных\nПополнение баланса")
 
 
@@ -37,27 +39,26 @@ class OnlineOperATM(MainATM):
 
     def online_perevod(self):
         print("******************************************************")
-        print(f"ID банкомата:{self._id_atm}\nОперация перевод-онлайн.")
+        print(f"{self.__NAME_BANK} ID банкомата:{self._id_atm}\nОперация перевод-онлайн.")
         inform = input("Введите номер телефона/карты или счёта получателя: ")
         summa = input("Введите сумму перевода: ")
+        return inform, summa
 
-    def inf1_atm(self):
-        super().inf1_atm()
+    def list_operation(self):
+        super().list_operation()
         print(f"Онлайн-операция.Перевод")
 
 
-NAMEBANK = "ТСБ"
-print(f"Банкоматы {NAMEBANK}:")
 ATM1 = AcceptGiveCash("12340", 500000)
 #ATM1.deposit_money()
 ATM2 = AcceptGiveCash("12341", 300000)
-#ATM2.inf_atm()
+#ATM2.inf_balance()
 ATM3 = OnlineOperATM("2102314", 1500000)
 #ATM3.online_perevod()
 ATM4 = OnlineOperATM("2102315", 100000)
-#ATM4.withdraw_money()
+ATM4.withdraw_money()
 
 ATM = {ATM1, ATM2, ATM3, ATM4}
 for i in ATM:
-    i.inf_atm()
-    i.inf1_atm()
+    i.inf_balance()
+    i.list_operation()
