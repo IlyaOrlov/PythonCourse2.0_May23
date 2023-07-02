607514657cd9a5f72d0ebcd39646d6eb8e4a6c0f
Андрей <kingfedor@mail.ru>
2023-07-02 23:54:32 +0300

    update2

--- Practice/Fedorov/HW_8/8.3.py
+++ Practice/Fedorov/HW_8/8.3.py
@@ -5,9 +5,10 @@ import time
 class MangerContext:
 
     def __init__(self):
-        self.new_now = dt.datetime.now()
+        self.new_now = None
 
     def __enter__(self):
+        self.new_now = dt.datetime.now()
         print(self.new_now)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
