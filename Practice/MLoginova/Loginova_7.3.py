9a1dc39e26e0530e3f45f6785b1f862a22834ddc
Maria Loginova <marria_loginova@mail.ru>
2023-07-02 15:06:03 +0300

    Tasks 7.3 Edit 1

--- Practice/MLoginova/Loginova_7.3.py
+++ Practice/MLoginova/Loginova_7.3.py
@@ -4,60 +4,60 @@ class MainATM:
         self._id_atm = id_atm
         self._balance = balance
 
-    def View_Balance(self):
+    def withdraw_money(self):
         print("******************************************************")
-        print(f"Просмотр баланса.ID банкомата:{self._id_atm} Баланс: {self._balance}")
-
-    def Withdraw_Money(self):
-        print("******************************************************")
-        money = int(input(f"ID банкомата:{self._id_atm} Выбрана операция снятие наличных.\nУкажите сумму, которую хотите снять: "))
+        money = int(input(f"ID банкомата:{self._id_atm} Выбрана операция снятие наличных."
+                          f"\nУкажите сумму, которую хотите снять: "))
         if self._balance > money:
             self._balance -= money
             print(f"Выдача наличных  - {money}")
         else:
             print(f"В банкомате недостаточно ср-в.")
 
-    def Deposit_Money(self):
+    def deposit_money(self):
         print("******************************************************")
-        money = int(input(f"ID банкомата:{self._id_atm} Выбрана операция пополнения наличных.\nУкажите сумму, которую хотите внести: "))
+        money = int(input(f"ID банкомата:{self._id_atm} Выбрана операция пополнения наличных."
+                          f"\nУкажите сумму, которую хотите внести: "))
         self._balance += money
         print(f"Внесена сумма  - {money}")
 
-    def Inf_ATM(self):
+    def inf_atm(self):
         print("******************************************************")
         print(f"ID банкомата:{self._id_atm}, баланс - {self._balance}.")
 
-    def Inf1_ATM(self):
+    def inf1_atm(self):
         print(f"Cписок поддерживаемых операций: \nПросмотр баланса\nВыдача наличных\nПополнение баланса")
 
 
-class AсceptGiveCash(MainATM):
+class AcceptGiveCash(MainATM):
     pass
+
 
 class OnlineOperATM(MainATM):
 
-    def Online_Perevod(self):
+    def online_perevod(self):
         print("******************************************************")
         print(f"ID банкомата:{self._id_atm}\nОперация перевод-онлайн.")
         inform = input("Введите номер телефона/карты или счёта получателя: ")
         summa = input("Введите сумму перевода: ")
 
-    def Inf1_ATM(self):
-        super().Inf1_ATM()
+    def inf1_atm(self):
+        super().inf1_atm()
         print(f"Онлайн-операция.Перевод")
+
 
 NAMEBANK = "ТСБ"
 print(f"Банкоматы {NAMEBANK}:")
-ATM1 = AссeptGiveCash("12340", 500000)
-#ATM1.Deposit_Money()
-ATM2 = AссeptGiveCash("12341", 300000)
-#ATM2.View_Balance()
+ATM1 = AcceptGiveCash("12340", 500000)
+#ATM1.deposit_money()
+ATM2 = AcceptGiveCash("12341", 300000)
+#ATM2.inf_atm()
 ATM3 = OnlineOperATM("2102314", 1500000)
-#ATM3.Online_Perevod()
+#ATM3.online_perevod()
 ATM4 = OnlineOperATM("2102315", 100000)
-ATM4.Withdraw_Money()
+#ATM4.withdraw_money()
 
 ATM = {ATM1, ATM2, ATM3, ATM4}
 for i in ATM:
-    i.Inf_ATM()
-    i.Inf1_ATM()
+    i.inf_atm()
+    i.inf1_atm()
