598998c77b2714b155c97a7532fffb1ff969b6a7
Pripisnov <prialexey@mail.ru>
2023-07-03 21:34:20 +0300

    Лекция5(исправления)

--- Practice/Pripisnov/work/my5.4work.py
+++ Practice/Pripisnov/work/my5.4work.py
@@ -1,17 +1,20 @@
 def remove_column(m, d):
-    num_columns = len(m[0])
-    new_matrix = [[row[i] for i in range(num_columns) if row[i] != d] for row in m]
-    return new_matrix
+    for row in m:
+        if d in row:
+            column_index = row.index(d)
+            for i in range(len(m)):
+                del m[i][column_index]
+            return m
+    return m
 
 
-my_matrix = [
+my_m = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ]
-digit_to_remove = 2
 
-result_matrix = remove_column(my_matrix, digit_to_remove)
-print("Матрица после удаления столбца с цифрой", digit_to_remove)
-for row in result_matrix:
-    print(row)
+print(my_m)
+b = int(input("Будет удален столбец с цифрой: "))
+result = remove_column(my_m, b)
+print(result)
