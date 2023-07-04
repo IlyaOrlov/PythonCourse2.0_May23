2dfb3fc861f8f3cff988fa78f832a81f6237b317
AnnaKapralova <annyadmitrievna@mail.ru>
2023-07-04 14:52:09 +0300

    Исправила 7.3

--- Practice/AKapralova/KapralovaDz7.3.py
+++ Practice/AKapralova/KapralovaDz7.3.py
@@ -6,20 +6,8 @@ class ATM:
     def about_atm(self):
         print(f"Банкомат: {self._name} Баланс: {self._balance}")
 
-
-class GetATM(ATM):
-    @staticmethod
-    def get_operation():
-        print("Банкомат может осуществлять операцию получения наличных.")
-
     def get_money(self, money):
         self._balance += money
-
-
-class GiveATM(ATM):
-    @staticmethod
-    def give_operation():
-        print("Банкомат может осуществлять операцию выдачи наличных.")
 
     def give_money(self, money):
         if self._balance - money < 0:
@@ -28,33 +16,30 @@ class GiveATM(ATM):
             self._balance -= money
 
 
+class GetGiveATM(ATM):
+    @staticmethod
+    def operation():
+        print("Банкомат может осуществлять операцию получения и выдачи наличных.")
+
+
 class OnlineATM(ATM):
     @staticmethod
     def online_operation():
-        print("Банкомат может осуществлять только онлайн операции.")
+        print("Банкомат может осуществлять операцию получения и выдачи наличных, а так же онлайн платежи")
 
     def online_payment(self):
-        print(f'{self._name}: Совершен онлайн-платеж')
+        print(f'{self._name}: Совершен онлайн платеж')
 
 
-b1 = GetATM("Сбер", 0)
-b1.about_atm()
-b1.get_operation()
-b1.get_money(200)
-b1.about_atm()
+b1 = GetGiveATM("Сбер", 1000)
+b1.operation()
 
-b2 = GiveATM("Альфа", 1000)
-b2.about_atm()
-b2.give_operation()
-b2.give_money(100)
-b2.about_atm()
+b2 = OnlineATM("ВТБ", 2000)
+b2.online_operation()
+b2.online_payment()
 
-b3 = OnlineATM("ВТБ", 0)
-b3.about_atm()
-b3.online_operation()
-b3.online_payment()
-b3.about_atm()
-
-# lst = [b1, b2, b3]
-# for i in lst:
-#     i.about_atm()
+lst = [b1, b2]
+for i in lst:
+    i.get_money(100)
+    i.give_money(200)
+    i.about_atm()
