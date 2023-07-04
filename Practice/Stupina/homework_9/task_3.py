297ba7a5bbcc13bd319cb6fbaf89a043c710e9b8
Alena <alenadv91@mail.ru>
2023-07-04 11:07:37 +0300

    дз №9, исправлена задача№3

--- Practice/Stupina/homework_9/task_3.py
+++ Practice/Stupina/homework_9/task_3.py
@@ -6,23 +6,19 @@ import datetime
 def show_dir(cur_dir):
     for i in os.listdir(cur_dir):
         path = os.path.join(cur_dir, i)
-        print(f'путь {path}')
         d_creation = os.path.getctime(path)
         d_creation = datetime.datetime.fromtimestamp(d_creation)
         d_exist = datetime.datetime.today() - d_creation
-
         if os.path.isfile(path):
             print(f"Файл: {i}")
             if d_exist > datetime.timedelta(minutes=1):
                 os.remove(path)
-                print(f'Удалили {i}')
         else:
             print(f"Папка: {i}")
             if next(os.scandir(path), None):
                 show_dir(path)
             if d_exist > datetime.timedelta(minutes=2) and not next(os.scandir(path), None):
                 shutil.rmtree(path)
-                print(f'Удалили {i}')
 
 
 s = os.path.join('.', 'my_dir')
