6ca55620199970a188f2c25913a0de3272d6c891
Maria Loginova <marria_loginova@mail.ru>
2023-07-06 17:02:50 +0300

    Tasks 8.3 Edit 2

--- Practice/MLoginova/Loginova_8.3.py
+++ Practice/MLoginova/Loginova_8.3.py
@@ -1,16 +1,17 @@
 #  Напишите свой менеджер контекста, замеряющий и показывающий время исполнения кода внутри него.
-from datetime import datetime
+from datetime import datetime as dt
 import random
 
 
 class TestTime:
     def __enter__(self):
-        self.start_time = datetime.now()
+        self.start_time = dt.now()
         print(f"Time begin: {self.start_time}")
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print(f"Time end: {datetime.now()}")
-        res = datetime.now() - self.start_time
+        time_end = dt.now()
+        print(f"Time end: {time_end}")
+        res = time_end - self.start_time
         print(f'Время выполнения:{res}')
 
 
