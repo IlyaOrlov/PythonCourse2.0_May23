f41e7eb658c87b053a599373c4df02f74b69cbc1
Ksenia Ozerova <ruchkinaks944@gmail.com>
2023-07-11 16:43:08 +0300

    task1

---
+++ Practice/Ozerova/5/5.1.py
@@ -0,0 +1,21 @@
+def minimal(x):
+    new_min = float("inf")
+    n = 0
+    for i, j in enumerate(x):
+        if j < new_min:
+            n = i
+            new_min = j
+    return n, new_min
+
+
+arr = [0, 3, 24, 2, 3, 7]
+
+a = 0
+while a < len(arr):
+    b, c = minimal(arr[a:])
+    if b != 0:
+        arr[a], arr[b + a] = arr[b + a], arr[a]
+    a += 1
+
+
+print(arr)
