eafe8f42e05773ea1b7412728cdd90b6ebf386dd
Svetlova <e.as@mail.ru>
2023-07-12 10:44:18 +0300

    Исправлено 7.2,7.3

--- Practice/Svetlova/DZ7.2.py
+++ Practice/Svetlova/DZ7.2.py
@@ -1,12 +1,27 @@
-import random
 import time
+from random import randint
 
-class Pupil:
+class Man:
+    def __init__(self, name):
+        self.name = name
+
     def solve_task(self):
-        delay = random.randint(3, 6)  # Генерируем случайную задержку от 3 до 6 секунд
-        time.sleep(delay)
-        print("Срабатывание")
+        print("Я еще не готов")
 
-# Пример использования
-pupil = Pupil()
+class Pupil(Man):
+    def solve_task(self):
+        thinking_time = randint(3, 6)
+        print(f"{self.name} размышляет...")
+        time.sleep(thinking_time)
+        print(f"{self.name} решил/а задачу")
+
+# Пример использования классов
+man = Man("Джон")
+man.solve_task()  # Вывод: "Я еще не готов"
+
+pupil = Pupil("Алиса")
 pupil.solve_task()
+# Вывод:
+# "Алиса размышляет..."
+# [ожидание случайного времени от 3 до 6 секунд]
+# "Алиса решила задачу"
