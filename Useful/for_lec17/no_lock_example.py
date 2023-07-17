13a21a4ddcb756819f32f7785151dd3677e48933
IlyaOrlov <orlov.soft.company@gmail.com>
2023-07-17 19:55:39 +0300

    ForLec17

--- Useful/for_lec17/no_lock_example.py
+++ Useful/for_lec17/no_lock_example.py
@@ -1,5 +1,5 @@
 # Два потока запрашивают и выводят координаты точки на плоскости
-from threading import Thread, RLock
+from threading import Thread
 import time
 
 
@@ -8,36 +8,24 @@ class Base:
     y = 0
 
 
-lock = False
-
-def print_fun(r):
+def print_fun():
     i = 0
-    global lock
     while i < 2:
-        # while lock is True:
-        #     time.sleep(1)
-        # lock = True
-        with r:
-            Base.x = input("2x: ")
-            Base.y = input("2y: ")
-            print(f'Thread 2: Base.x = {Base.x}, Base.y = {Base.y}')
+        Base.x = input("2x: ")
+        Base.y = input("2y: ")
+        print(f'Thread 2: Base.x = {Base.x}, Base.y = {Base.y}')
         i += 1
         time.sleep(1)
 
 
 if __name__ == '__main__':
-    r = RLock()
-    t = Thread(target=print_fun, args=(r,))
+    t = Thread(target=print_fun)
     t.start()
     i = 0
     while i < 2:
-        # while lock is True:
-        #     time.sleep(1)
-        # lock = True
-        with r:
-            Base.x = input("1x: ")
-            Base.y = input("1y: ")
-            print(f'Thread 1: Base.x = {Base.x}, Base.y = {Base.y}')
+        Base.x = input("1x: ")
+        Base.y = input("1y: ")
+        print(f'Thread 1: Base.x = {Base.x}, Base.y = {Base.y}')
         i += 1
         time.sleep(1)
     t.join()
