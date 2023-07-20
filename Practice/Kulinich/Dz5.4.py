d2ac72fbc621e876ff9b6b4d352e493e5a1710d3
Lud-mila88 <Lud-mila88@mail.ru>
2023-07-21 00:30:21 +0300

    Dz5.4

---
+++ Practice/Kulinich/Dz5.4.py
@@ -0,0 +1,24 @@
+def transp(arr):
+    arrTrans = [[0 for j in range(len(arr))] for i in range(len(arr[0]))]
+    for i in range(len(arr)):
+        for j in range(len(arr[0])):
+            arrTrans[j][i] = arr[i][j]
+    return arrTrans
+
+
+def DelColumn(arr,a):
+    delindex = []
+    for index, i in enumerate(arr):
+        for index1, j in enumerate(i):
+            if j == a:
+                delindex.append(index1)
+    delindex.sort()
+    arr = transp(arr)
+    for i in range(len(delindex) - 1, -1, -1):
+        del arr[delindex[i]]
+    arr = transp(arr)
+    return print(arr)
+
+
+matrix = [[9,2,5],[4,5,6],[7,8,9]]
+DelColumn(matrix,5)
