b8e97f2c3e35f8b8ce5e552afb01f99418ab1edc
mihailkisunkin <mkisunkin@gmail.com>
2023-08-01 15:22:25 +0300

    Второй вариант решения 4.5

--- Practice/Kisunkina/lec 5/homework 5.1.py
+++ Practice/Kisunkina/lec 5/homework 5.1.py
@@ -8,4 +8,23 @@
 # // здесь реализованный алгоритм
 # // на выходе должен получиться список, содержащий [0, 2, 3, 3, 7, 24]
 
+def sort_arr(arr):
+    for indx in range(len(arr)):
+        ost_indx = indx + 1
+        min_elem = arr[indx]
+        for i in range(len(arr[ost_indx:])):
+            if arr[ost_indx] < min_elem:
+                min_elem = arr[ost_indx]
+                arr[indx], arr[ost_indx] = arr[ost_indx], arr[indx]
+            ost_indx +=1
+    print(f'{arr}')
+    return arr
+
+
+arr = [0,3,24,2,3,7]
+
+
+if __name__ == '__main__':
+    sort_arr(arr)
+
 
