6a2ebe44fc43ed7ee79e9bc0898e2e9addd1f16a
mihailkisunkin <mkisunkin@gmail.com>
2023-08-02 21:11:27 +0300

    Задача 5.2

---
+++ Practice/Kisunkina/lec 5/homework 5.2.py
@@ -0,0 +1,16 @@
+# Написать и вызвать функцию, возвращающую первый повторившийся символ в переданном списке.
+# Например, для  списка [2, 3, 4, 5, 3, 2] функция должна вернуть 3.
+
+def compare_arg(arr):
+    arr2 = []
+    for elem in arr:
+        if elem in arr2:
+            print(f'Первое повторяющееся число {elem}')
+            return elem
+        else:
+            arr2.append(elem)
+
+
+if __name__ == '__main__':
+    arr = [2, 3, 4, 5, 3, 2]
+    compare_arg(arr)
