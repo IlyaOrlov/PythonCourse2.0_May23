a3a379f75be073399cc80232c0afbfc664826d3a
Maria Loginova <marria_loginova@mail.ru>
2023-08-02 18:10:24 +0300

    Tasks 14.1 Edit 1

--- Practice/MLoginova/Loginova_14.py
+++ Practice/MLoginova/Loginova_14.py
@@ -1,24 +1,20 @@
 class MyError(Exception):
-    def __init__(self, text):
-        self.txt = text
+    pass
 
 class MyFun:
-    def __init__(self, number):
-        self.number = number
+    __roman_numbers = {'M': 1000, 'CM': 900, 'D': 500, 'CD': 400,
+                       'C': 100, 'XC': 90, 'L': 50, 'XL': 40,
+                       'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1}
 
-    def to_roman(self):
+    def to_roman(self, number):
         roman = ''
-        if not isinstance(self.number, int) and self.number not in range(1, 5001):
+        if not isinstance(number, int) or number not in range(1, 5001):
             raise MyError('NonValidInput')
-        for i, value in roman_numbers.items():
-            while self.number >= value:
+        for i, value in self.__roman_numbers.items():
+            while number >= value:
                 roman += i
-                self.number -= value
+                number -= value
         return roman
 
 
-roman_numbers = {'M': 1000, 'CM': 900, 'D': 500, 'CD': 400,
-                 'C': 100, 'XC': 90, 'L': 50, 'XL': 40,
-                 'X': 10, 'IX': 9, 'V': 5, 'IV': 4, 'I': 1}
-m1 = MyFun(566)
-print(m1.to_roman())
+m1 = MyFun()
--- Practice/MLoginova/tests.py
+++ Practice/MLoginova/tests.py
@@ -3,14 +3,18 @@ import Loginova_14
 
 
 class TestMyProv:
-    @pytest.fixture(
-        scope='function',
-        params=[(566, 'DLXVI'), ('7', 'NonValidInput'), (-5, 'NonValidInput'), (10000000, 'NonValidInput')],
-        ids=lambda args: f"Test fun with args: {args}"
-    )
-    def parametrize_sum(self, request):
-        return request.param
 
-    def test_my_sum(self, parametrize_sum):
-        res = Loginova_14.m1.to_roman()
-        assert(res, parametrize_sum)
+    def test_to_roman(self):
+        assert (Loginova_14.m1.to_roman(3459) == 'MMMCDLIX')
+
+    def test_my_error1(self):
+        with pytest.raises(Loginova_14.MyError):
+            Loginova_14.m1.to_roman('3459')
+
+    def test_my_error2(self):
+        with pytest.raises(Loginova_14.MyError):
+            Loginova_14.m1.to_roman(5521)
+
+    def test_my_error3(self):
+        with pytest.raises(Loginova_14.MyError):
+            Loginova_14.m1.to_roman(-2)
