e1d83a914f5fa9da255287ad8b64e49f3a4f66b7
Maria Loginova <marria_loginova@mail.ru>
2023-08-03 14:10:34 +0300

    Tasks 13.2 Edit 2

--- Practice/MLoginova/Loginova_13.2.py
+++ Practice/MLoginova/Loginova_13.2.py
@@ -1,5 +1,4 @@
 import mongoengine as me
-import os
 
 
 class RegistryUL(me.Document):
@@ -16,34 +15,38 @@ class RegistryUL(me.Document):
 
 
 if __name__ == "__main__":
-    db_name = 'test1'
-    db_exists = os.path.exists(db_name)
-
+    db_name = 'test'
     with (me.connect(db_name)) as conn:
-        if not db_exists:
-            org1 = RegistryUL(inn='1234567890',
-                              name_organ='SUN',
-                              type_ownership='ООО',
-                              name_owner='Иванов И.И.',
-                              address='Россия, г.Нижний Новгород, ул. Горького, д.65')
-            org1.save()
-    #   Создаем документ2
-            org2 = RegistryUL(inn='1234567891',
-                              name_organ='Оазис',
-                              type_ownership='ОАО',
-                              name_owner='Петров И.И.',
-                              address='Россия, г.Нижний Новгород, ул. Ильниская, д.45')
-            org2.save()
-    #   Создаем документ3
-            org3 = RegistryUL(inn='1234567892',
-                              name_organ='Генезис',
-                              type_ownership='ПАО',
-                              name_owner='Сидоров И.И.',
-                              address='Россия, г.Нижний Новгород, ул. Обухова, д.20')
-            org3.save()
-        print(f"Документов в базе:{RegistryUL.objects.count()}")
-        input_inn = ' '
-        while len(input_inn := input("Введите ИНН ЮЛ(10 символов): ")) != 10:
-            print(f"Вы ввели ИНН из {len(input_inn)} символов, нужно из 10. Повторите ввод!")
-        print(f"Информация по ЮЛ по ИНН {input_inn}:\n {RegistryUL.objects.filter(inn=input_inn)}")
-        #   RegistryUL.objects.delete()
+        if RegistryUL.objects.count() == 0:
+            if (k := input("Отсутствуют документы в БД. Заполнить тестовыми данными? (y/n)\n")) == 'y':
+                org1 = RegistryUL(inn='1234567890',
+                                  name_organ='SUN',
+                                  type_ownership='ООО',
+                                  name_owner='Иванов И.И.',
+                                  address='Россия, г.Нижний Новгород, ул. Горького, д.65')
+                org1.save()
+                org2 = RegistryUL(inn='1234567891',
+                                  name_organ='Оазис',
+                                  type_ownership='ОАО',
+                                  name_owner='Петров И.И.',
+                                  address='Россия, г.Нижний Новгород, ул. Ильниская, д.45')
+                org2.save()
+                org3 = RegistryUL(inn='1234567892',
+                                  name_organ='Генезис',
+                                  type_ownership='ПАО',
+                                  name_owner='Сидоров И.И.',
+                                  address='Россия, г.Нижний Новгород, ул. Обухова, д.20')
+                org3.save()
+                print(f"Документов в базе:{RegistryUL.objects.count()}.")
+                input_inn = ' '
+                while len(input_inn := input("Введите ИНН ЮЛ(10 символов): ")) != 10:
+                    print(f"Вы ввели ИНН из {len(input_inn)} символов, нужно из 10. Повторите ввод!")
+                print(f"Информация по ЮЛ по ИНН {input_inn}:\n {RegistryUL.objects.filter(inn=input_inn)}")
+            else:
+                print(f"Документов в базе:{RegistryUL.objects.count()}. Выход")
+        else:
+            print(f"Документов в базе:{RegistryUL.objects.count()}.")
+            input_inn = ' '
+            while len(input_inn := input("Введите ИНН ЮЛ(10 символов): ")) != 10:
+                print(f"Вы ввели ИНН из {len(input_inn)} символов, нужно из 10. Повторите ввод!")
+            print(f"Информация по ЮЛ по ИНН {input_inn}:\n {RegistryUL.objects.filter(inn=input_inn)}")
