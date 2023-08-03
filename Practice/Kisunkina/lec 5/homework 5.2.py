b5a30507fc35ee887ecccd005fca0b358e32aafc
mihailkisunkin <mkisunkin@gmail.com>
2023-08-03 14:33:07 +0300

    Задача 5.2 правка

--- Practice/Kisunkina/lec 5/homework 5.2.py
+++ Practice/Kisunkina/lec 5/homework 5.2.py
@@ -2,13 +2,13 @@
 # Например, для  списка [2, 3, 4, 5, 3, 2] функция должна вернуть 3.
 
 def compare_arg(arr):
-    arr2 = []
+    arr2 = set()
     for elem in arr:
         if elem in arr2:
             print(f'Первое повторяющееся число {elem}')
             return elem
         else:
-            arr2.append(elem)
+            arr2.add(elem)
 
 
 if __name__ == '__main__':
