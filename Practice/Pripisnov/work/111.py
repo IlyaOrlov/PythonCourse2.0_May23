135a98d388272257fa14213bb3e88cdef78eb4d7
Pripisnov <prialexey@mail.ru>
2023-08-10 21:06:31 +0300

    commit

--- Practice/Pripisnov/work/111.py
+++
@@ -1,9 +0,0 @@
-arr = [0,3,24,2,3,7]
-i = 0
-while i < len(arr):
-    m = min(arr[i:])
-    ind =arr.index(m)
-    arr[i], arr[ind] = arr[ind], arr[i]
-    i += 1
-
-print(m)
--- Practice/Pripisnov/work/tak/tak6.py
+++
@@ -1,8 +0,0 @@
-import random
-
-
-
-for i in range(50):
-    name = f"file_{i}"
-    with open(name, "w") as f:
-        f.write(str(random.randint(1, 1000)))
