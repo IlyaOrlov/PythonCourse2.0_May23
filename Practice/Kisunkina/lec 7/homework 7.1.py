5e9881e9e108ab3b573692637ddd8c31115a64b7
mihailkisunkin <mkisunkin@gmail.com>
2023-08-10 14:00:53 +0300

    Задачи 7.1 и 7.2

---
+++ Practice/Kisunkina/lec 7/homework 7.1.py
@@ -0,0 +1,13 @@
+# Написать класс Man, который принимает имя в конструкторе. Имеет метод solve_task,
+# который просто выводит "I'm not ready yet".
+
+class Man:
+    def __init__(self, name):
+        self.name = name
+
+    @staticmethod
+    def solve_task():
+        print("I'm not ready yet")
+
+men = Man("Шейх Тамим")
+men.solve_task()
