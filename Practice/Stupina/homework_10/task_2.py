f934dae43b3bf3e7ea11e989966beba8258372b0
Alena <alenadv91@mail.ru>
2023-08-11 20:23:24 +0300

    дз №10,задача 2 исправлено

--- Practice/Stupina/homework_10/task_2.py
+++ Practice/Stupina/homework_10/task_2.py
@@ -2,39 +2,27 @@ from multiprocessing import Pool
 import os
 
 
-lst = []
-
-
 def my_sum(*args):
-    lst.append(args)
-    print(f'This process {os.getpid()} processed values {lst}')
-
+    print(f'This process {os.getpid()} ')
     if args:
         s = args[0]
         for i in args[1:]:
             s += i
     else:
         s = None
-
     return s
 
 
 if __name__ == '__main__':
+    lst_1, lst_2, lst_3 = [], [], []
 
-    lst_1 = []
-    lst_2 = []
-    lst_3 = []
-
-    par_0 = [i for i in range(100)]
-
-    for i in range(20):
+    for i in range(1000000):
         lst_1.append(i)
         lst_2.append(i**2)
         lst_3.append(chr(i+100))
 
-    par = [tuple(par_0), (lst_1, lst_2, lst_3), ('abc', 'defgh','iklmnopqrst')]
+    par = [range(10000000), (lst_1, lst_2, lst_3), ('abc', 'defgh', 'iklmnopqrst')]
 
     count_p = len(par)
     pool = Pool(processes=count_p)
     res = pool.starmap(my_sum, par)
-    print(res)
